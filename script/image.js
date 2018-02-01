3d515e04653eec727b06716807d4124d55a6092e
dewysoekhoe <dewy.soekhoe@gmail.com>
2018-02-01 10:50:51 +0100

    script

--- script/image.js
+++ script/image.js
@@ -1,14 +1,17 @@
+/*variabele aanmaken*/
 var images = document.querySelectorAll('.toggle-image');
 
+/*aanroepen van image*/
 function toggleImage(event) {
   var clickedImage = event.target;
-	var currentSource = clickedImage.src;
+  var currentSource = clickedImage.src;
   var targetSource = clickedImage.getAttribute('data-alt-src');
   
   clickedImage.src = targetSource;
   clickedImage.setAttribute('data-alt-src', currentSource);
 }
 
+/*image wordt veranderd na een klik*/
 images.forEach(function(image) {
   image.addEventListener('click', toggleImage);
 });
